90c0b49538912607404a588d3717517461c4ba5c
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:51:11 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HOME';
+export const nameSongs = [
+	"HEAD FIRST",
+	"RESONANCE",
+	"PYXIS",
+	"DREAM HEAD",
+	"NATIVE",
+	"DECAY",
+	"TWISTED LIGHT",
+	"WE’RE FINALLY LANDING",
+	"STILL LIFE",
+	"SOAK",
+	"R.I.S.E",
+	"HOLD",
+	"BEFORE THE NIGHT",
+	"BURNING",
+	"12",
+	"MAINFRAME",
+	"OORT CLOUD",
+	"TIDES",
+	"NIGHTS",
+	"NEW MACHINES"
+]
